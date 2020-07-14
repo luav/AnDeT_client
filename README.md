8da2cf85f52e150d6818cc49a948116ac7e6539e
Artem <luardev@gmail.com>
2020-07-14 14:07:35 +0200

    Project briefly described, build refined

--- README.md
+++ README.md
@@ -1,22 +1,44 @@
 # AnDeT_client
-Client Applications for AnDeT (Ant Development Tracker)
+This is a collection of client applications for AnDeT (Ant Development Tracker) at the Department of Biology of the UNIFR.  
+AnDeT is an extension of [FORT (Formicidae Tracker)](https://github.com/formicidae-tracker) of the UNIL.
+
+Website: https://www3.unifr.ch/bio/en/groups/leboeuf-group/  
+Author of the extensions: Artem Lutov <lutov.analytics@gmail.com>
 
 > To clone this repository with submodules use:
 > ```
 > $ git clone --recursive <repo_url>
 > ```
-> Anyway, all submodules are fetched automatically when installing requirements via the build script.
+> Anyway, all submodules are fetched automatically when installing requirements via the `build` script.
 
 Components:
-- leto (client: leto-cli) to watch live tracking video and configure the tracking itself;
-- tag-layouter to draw tracking tags;
-- studio to analyze tracking data.
+- `leto-cli` to watch live tracking video and configure the tracking itself;
+- `tag-layouter` to draw tracking tags;
+- `studio` to analyze tracking data.
 
 ## Requirements
-Linux Debian 9+ / Ubuntu 18+
+Linux Debian 9+ / Ubuntu 18+.
+> All required libraries are installed automatically during the initial build: `./build -i`.
 
 ## Build
-To build all components:
+To build all components for the first time, installing all requirements:
 ```
-$ ./build.sh
+$ ./build.sh -i
+```
+
+For the subsequent builds of all components, or of the specified one:
+```
+$ ./build.sh [<component>]
+```
+
+Details:
+```
+$ ./build.sh -h | [-i,--init] [<component>=ALL]
+  -h,--help  - help, show this usage description
+  -i,--init  - initialize the build environment, which should be done only when building the project for the first time
+  <component>  - {leto-cli, tag-layouter, studio, ALL}
+
+  Examples:
+  $ ./build.sh -i
+  $ ./build.sh tag-layouter
 ```
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # \description  Build client components of AnDeT (Ant Development Tracker)
 # Note: this script is designed for Linux Ubuntu and might also work on Debian
@@ -6,30 +6,120 @@
 #
 # \author Artem Lutov <lutov.analytics@gmail.com>
 
-# build [-i,--initial] [component]
-# components: leto-cli, tag-layouter, studio
+USAGE="$0 -h | [-i,--init] [<component>=ALL]
+  -h,--help  - help, show this usage description
+  -i,--init  - initialize the build environment, which should be done only when building the project for the first time
+  <component>  - {leto-cli, tag-layouter, studio, ALL}
 
-./install_reqs.sh
+  Examples:
+  \$ $0 -i
+  \$ $0 tag-layouter
+"
+
+INIT=0  # Whether to initialize the build environment
+TARG='ALL'  # Target component to be built
+while [ $1 ]; do
+	case $1 in
+	-h|--help)
+		# Use defaults for the remained parameters
+		#echo -e $USAGE # -e to interpret '\n\
+        printf "$USAGE"
+		exit 0
+		;;
+	-i|--init)
+		if [ "${2::1}" == "-" ]; then
+			echo "ERROR, invalid argument value of $1: $2"
+			exit 1
+		fi
+		INIT=1
+		echo "Build environment initialzation is activated"
+		shift
+		;;
+	*)
+        # Set th building component
+    	if [ $1 != 'ALL' ]; then
+            TARG=$1
+        fi
+		shift
+        if [ $1 ]; then
+            printf "Error: Invalid option specified: $1 ...\n\n$USAGE"
+            exit 1
+        fi
+		;;
+	esac
+done
+
+# Initialize the build environment if required
+if [ $INIT -ne 0 ]; then
+    ./install_reqs.sh
+
+    ERR=$?
+    if [ $ERR -ne 0 ]; then
+        echo "ERROR, installation of the build environment is failed, error code: $ERR"
+        exit $ERR
+    fi
+fi
+
 # Update all submodules
 git submodule update --recursive
 
-#go get -u github.com/formicidae-tracker/leto/leto-cli
+NBUILDS=0  # The number of all builds
+
 # Client for tracking configuration and live tracking video
-cd leto/leto-cli && \
-go build -i . && \
-cd ../..
+ERR_LETO=0
+PROJ='leto-cli'
+if [ $TARG == 'ALL' -o $TARG == $PROJ ]; then
+    echo "Building $PROJ ..."
+    #go get -u github.com/formicidae-tracker/leto/leto-cli
+    cd leto/$PROJ && \
+    go build -i . && \
+    cd ../..
+    ERR_LETO=$?
+    ((++NBUILDS))
+fi
 
 # Tag families drawing app
-cd tag-layouter && \
-git submodule update --init && \
-make
-# TODO: fix an error
-#make
+PROJ='tag-layouter'
+ERR_TAG=0
+if [ $TARG == 'ALL' -o $TARG == $PROJ ]; then
+    echo "Building $PROJ ..."
+    cd $PROJ && \
+    git submodule update --init && \
+    make
+    # TODO: fix an error
+    #make
+    ERR_TAG=$?
+    ((++NBUILDS))
+fi
 
 # FORT Studio and Myrmidon API to analyze tracking data
-cd studio
-# -p to omit error if the directory is already exists
-mkdir -p build && \
-cd build && \
-cmake .. && \
-make -j 4
+PROJ='studio'
+ERR_STUD=0
+if [ $TARG == 'ALL' -o $TARG == $PROJ ]; then
+    echo "Building $PROJ ..."
+    cd $PROJ
+    # -p to omit error if the directory is already exists
+    mkdir -p build && \
+    cd build && \
+    cmake .. && \
+    make -j 4
+    ERR_STUD=$?
+    ((++NBUILDS))
+fi
+
+# Report the build execution status
+if [ $ERR_LETO -ne 0 -o $ERR_TAG -ne 0 -o $ERR_STUD -ne 0 ]; then
+	echo "ERROR, some builds were failed, errorcodes:
+  leto-cli: $ERR_LETO
+  tag-layouter: $ERR_TAG
+  studio: $ERR_STUD
+"
+	exit 1
+fi
+
+if [ $NBUILDS -eq 0 ]; then
+    echo "ERROR: the specified component is unknown: $TARG"
+    exit 1
+fi
+
+echo "Successfully completed boulds: $NBUILDS"
